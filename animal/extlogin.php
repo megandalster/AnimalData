497cbd17dd43da39434aa056b6df77a12a37bfa2
allentucker <allentucker@10.0.0.240>
2019-07-03 14:22:08 -0400

    minor fix

--- animal/extlogin.php
+++ animal/extlogin.php
@@ -1,9 +1,9 @@
 <?php
 // HTTPSON
-if($_SERVER["HTTPS"] != "on") {
-   header("Location: https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]);
-   exit();
-}
+//if($_SERVER["HTTPS"] != "on") {
+//   header("Location: https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]);
+//   exit();
+//}
 // HTTPSOFF
 session_start();
 ?>
